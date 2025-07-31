28b93ec6c2894619b71e496bd958b6c94b0c1dcd
Ariel França <ariel.franca@hotmail.com>
2025-07-31 14:11:06 -0300

    feat: add test script for JWT token refresh functionality

---
+++ teste.py
@@ -0,0 +1,9 @@
+import requests
+
+headers = {
+  "Authorization": "Bearer Token123"
+}
+
+requisicao = requests.get("http://127.0.0.1:8000/auth/refresh", headers=headers)
+print(requisicao)
+print(requisicao.json)
