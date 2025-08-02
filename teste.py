9e592482548504b2d6ecb8bf4bb81ec01a9fb620
Ariel França <ariel.franca@hotmail.com>
2025-08-01 22:31:16 -0300

    refactor

--- teste.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers = {
-  "Authorization": "Bearer Token123"
-}
-
-requisicao = requests.get("http://127.0.0.1:8000/auth/refresh", headers=headers)
-print(requisicao)
-print(requisicao.json)
