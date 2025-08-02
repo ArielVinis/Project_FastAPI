a852b548be2f5e03e17a21f706bbac9fa03e2b84
Ariel França <ariel.franca@hotmail.com>
2025-08-01 22:31:46 -0300

    feat: add home route with success message and documentation link, and update import statements for clarity

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # Para rodar o código, executar no terminal: uvicorn main:app --reload
-# Realizar um migration com alembic: alembic revision --autogenerate -m "Mensagem"
+# Lembrar sempre do processo de Lazyloaded na rotas ao mostrar as mensagens no json.
+# Para gerar o documento(requeriments.txt) com as dependencias usadas: pip freeze > requirements.txt
+# Para instalar o requeriments.txt: pip install -r requirements.txt
 
 from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
+from fastapi.responses import HTMLResponse
+
+from starlette.responses import HTMLResponse
 
 load_dotenv()
 
@@ -18,8 +23,14 @@ app = FastAPI()
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login_form")
 
-from routes.auth_routes import auth_router
-from routes.order_routes import order_router
+from routes import auth_router, order_router
 
 app.include_router(auth_router)
 app.include_router(order_router)
+
+@app.get("/")
+def home():
+    return {
+      "mensagem": "🚀 API rodando com sucesso!",
+      "documentation_swagger": "http://localhost:8000/docs"
+    }
